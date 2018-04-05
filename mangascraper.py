2c1210d899b1db5e4b23e352b268190ee38df830
barrettotte <barrettotte@gmail.com>
2018-04-04 20:01:48 -0400

    Added searching and exception handling

--- mangascraper.py
+++ mangascraper.py
@@ -5,16 +5,7 @@
 #   - BeautifulSoup4
 #   - PIP
 #   - Requests
-
-# Goals
-#   -  Scrape MangaDex or MangaFox for web page information.
-#   -  Download image files of manga. (Requests and Shutil)
-#   -  Convert manga names appropriately
-#   -  Setup query correctly to retrieve correct files from name.
-#   -  Convert images to pdf or appropriate extension for viewing on KindleFire
-#   -  Create simple front end GUI
-#   -  Wrap project up for 'release'
-
+# Konjiki No Moji Tsukai - Yuusha Yonin Ni Makikomareta Unique Cheat <-- Test for downloading all even if some chapter are missing
 
 
 from bs4 import BeautifulSoup as bs  # Source code parsing
@@ -24,44 +15,38 @@ import requests # HTTP Requests
 import shutil #Advanced file handling
 import os # Directory operations
 from sys import platform # check platform
-from fpdf import FPDF
 import glob
+import zipfile
 
 
 #Constants
 PROVIDER = "https://www.mangareader.net/"
-INITIAL_PAGE = 1
-SUCCESS_MSG = "\tDownloaded successfully!"
-DOWNLOAD_MSG = "\tDownloaded page #"
-NOTFOUND_MSG = "Queried Manga not found."
-REQUEST_ERR = "Requested page could not be reached." 
 FILE_EXT = ".jpg" 
+PROGRAM_TITLE = "Barrett's Manga Downloader"
+
 
 
 
 def sendRequest(url, binary = False):
   try:
     request = requests.get(url, stream = binary)
+  except KeyboardInterrupt:
+    endProgram("\n  Keyboard Interruption detected.\n  Download interrupted.\n  CBZ file was not created.")
   except:
-    print(REQUEST_ERR)
-    exit()
+    endProgram("\n  Request could not be made.\n  Download Interrupted.\n  CBZ file was not created.")
   return request
+
 
 
 def downloadImage(url, downloadPath, page):
   if not os.path.exists(downloadPath):
     os.makedirs(downloadPath)
-
   imgName = addZeroes(str(page), 3) + FILE_EXT
   imgPath = downloadPath + imgName
   request = sendRequest(url, True)
-  
   with open(imgPath, 'wb') as filePath:
     request.raw.decode_content = True
     shutil.copyfileobj(request.raw, filePath)
-
-  print(DOWNLOAD_MSG + str(page))
-
 
 
 
@@ -71,8 +56,14 @@ def getDownloadPath(series, chapter, path):
 def getPageURL(series, chapter, page = 1):
   return PROVIDER + dashes(series) + "/" + str(chapter) + "/" + str(page)
 
-def dashes(series):
-  return re.sub("\s+", "-", series.lower())
+def getSearchURL(search):
+  return PROVIDER + "search/?w=" + spaceToPlus(search)
+
+def dashes(text):
+  return re.sub("\s+", "-", text.lower())
+
+def spaceToPlus(text):
+  return re.sub("\s+", "+", text.lower())
 
 def addZeroes(num, maxDigits):
   digits = len(num)
@@ -80,7 +71,10 @@ def addZeroes(num, maxDigits):
   return zeroes + num 
 
 def yesOrNo(question):
-  reply = str(input(question + ' (y/n): ')).lower().strip()
+  try:
+    reply = str(input(question + ' (y/n): ')).lower().strip()
+  except KeyboardInterrupt:
+      endProgram("\n  Keyboard Interruption detected.")
   if reply[0] == 'y':
     return True
   if reply[0] == 'n':
@@ -93,78 +87,180 @@ def clearScreen():
     os.system('cls')
   elif platform == "linux":
     os.system('clear')
-  
+
+def drawHeader():
+  print("-----------", PROGRAM_TITLE, "-------------")
+  print("\n  NOTE: Manga is currently only downloaded from mangareader.net")
 
 
-def downloadMangaChapter(seriesName, chapter, path):
+
+def downloadMangaChapter(seriesName, chapter, path, endRange):
   downloadPath = getDownloadPath(seriesName, chapter, path)
-  currentPage = INITIAL_PAGE
-  print ("  Retrieving", seriesName, "Chapter", chapter, "and saving to\n    " + downloadPath)
+  currentPage = 1
+  print ("\n  Retrieving", seriesName, "Chapter", chapter, "and saving to\n    " + downloadPath)
+  totalPages = 0
 
   # Loop through all pages of chapter until there are none left
-  while True:
+  while currentPage:
     pageURL = getPageURL(seriesName, chapter, currentPage)
     request = sendRequest(pageURL)
     rawHTML = request.text
 
     if request.status_code != 200 or not len(rawHTML):
-      print (NOTFOUND_MSG if not len(rawHTML) else SUCCESS_MSG)
       break
-
-    # Scrape the HTML
     parsedHTML = bs(rawHTML, "html.parser")
-    imgURL = parsedHTML.find("img", {"id" : "img"}).get("src")  
 
-    downloadImage(imgURL, downloadPath, currentPage)
+    # Get total pages
+    if currentPage == 1:
+      dropdown = parsedHTML.find("select", {"id" : "pageMenu"})
+      totalPages = int(len(dropdown) / 2)
+
+    imgURL = parsedHTML.find("img", {"id" : "img"}).get("src")
+    try:
+      downloadImage(imgURL, downloadPath, currentPage)
+    except KeyboardInterrupt:
+      endProgram("\n  Keyboard Interruption detected.\n  Download interrupted.\n  CBZ file was not created.")
+
+    print("\tDownloaded Page {} of {}".format(currentPage, totalPages), end = '\r')
     currentPage = currentPage + 1
 
 
-def createPDF(seriesName, chapter, path):
+
+def createCBZ(seriesName, chapter, path):
   downloadPath = getDownloadPath(seriesName, chapter, path)
-  imageList = glob.glob(downloadPath + "/*.jpg")
-  
-  
+  imageList = glob.glob(downloadPath + "/*" + FILE_EXT)
+  try:
+    zip = zipfile.ZipFile(downloadPath +  seriesName + "-" + addZeroes(str(chapter), 4) + ".cbz", mode='w')
+  except:
+    print("\n  Unable to find", seriesName, " Chapter", chapter, ". Unable to create a .CBZ file for it.")
+    return
+  try:
+    for i, image in enumerate(sorted(imageList)):
+      zip.write(image)
+  finally:
+    print("  Created CBZ File for", seriesName,"Chapter",chapter,"with",len(imageList),"pages")
+    zip.close()
 
-  pdf = FPDF('P', 'mm', (281.7, 413))
-  #pdf = FPDF('P', 'mm')
-  pdf.set_auto_page_break(False, 0.0)
-  pdf.set_margins(0.0, 0.0, 0.0)
-  pdf.set_display_mode('fullpage', 'single')
 
-  for image in sorted(imageList): 
-    pdf.add_page()
-    pdf.image(image)
+
+def fixResults(resultsRaw):
+  fixed = []
+  for result in resultsRaw:
+    rList = result.get_text()
+    rList = list(filter(None, rList))
+    for idx, r in enumerate(rList):
+      if '\xa0' in r or '\n' in r:
+        rList.remove(r)
+        rList[idx-1] += ' '
+    s = ''.join(rList).strip()
+    fixed.append( s[:50] + (s[50:]))
+  return fixed
+
+
+
+def searchForManga(search):
+  pageURL = getSearchURL(search)
+  request = sendRequest(pageURL)
+  rawHTML = request.text
+  parsedHTML = bs(rawHTML, "html.parser")
   
-  print("  Finished building PDF for", seriesName,"-Chapter", chapter, "with", len(imageList), "pages")
-  pdf.output(downloadPath + "/" + seriesName + "-" + addZeroes(str(chapter), 4) + ".pdf", "F")
+  resultsRaw = parsedHTML.find_all("div", {"class" : "manga_name"})
+  mangaNames = fixResults(resultsRaw)
+  
+  resultsRaw = parsedHTML.find_all("div", {"class" : "chapter_count"})
+  mangaChapters = fixResults(resultsRaw)
+  for idx,chapter in enumerate(mangaChapters):
+    mangaChapters[idx] = ''.join(chapter.split(" ", 1)[0]).strip()  # only get chapter count
 
+  print("\n  Search Returned", len(mangaNames), "result(s).\n")
+  for idx,r in enumerate(mangaNames):
+    print("\t[" + addZeroes(str(idx), 3) + "]  " + r + " --- " + mangaChapters[idx]+ " Chapters")
+  return mangaNames, mangaChapters
+
+
+
+def inputString(prompt):
+  while(True):
+    try:
+      x = input(prompt).title().rstrip().lstrip()
+    except KeyboardInterrupt:
+        endProgram("\n  Keyboard Interruption detected.")
+    if not x:
+      print("  Invalid input. Please retry. ")
+      continue
+    else:
+      return x
+
+
+
+def inputInteger(prompt, min, max):
+  while(True):
+    try:
+      x = input(prompt).replace(" ", "")
+    except KeyboardInterrupt:
+        endProgram("\n  Keyboard Interruption detected.")
+    try:
+      x = int(x)
+      if(x < min or x > max):
+        print("  Index not within range [" + str(min) + ", " + str(max) + "]")
+        continue
+      else:
+        return x
+    except ValueError:
+      print("  Invalid input. Please retry. ")
+      continue
+
+
+
+def endProgram(prompt = ""):
+  print (prompt + "\n  Ending Program.")
+  print ("  Thank you for using", PROGRAM_TITLE, "!")
+  try:
+    sys.exit(0)
+  except SystemExit:
+    os._exit(0)
+  
 
 
 def main(): 
   path = os.getcwd() + "/Manga/" 
+  clearScreen()
+  drawHeader()
 
   while True:
-    clearScreen()
-    print("----------- Manga Downloader V1 -------------")
-    print("\n  NOTE: Manga is currently only downloaded from mangareader.net") 
-    title = input("\n\tEnter Series Name: ").title()
-    chapterStart = int(input("\tEnter Chapter Range Start : "))
-    chapterEnd = int(input("\tEnter Chapter Range End : ")) 
-
-    print ("\tDownloading Chapters", chapterStart, "-", chapterEnd, "\n")
-
-    for chapter in range(chapterStart, chapterEnd+1):
-      downloadMangaChapter(title, chapter, path) 
-      print("  Finished Downloading",title, "Chapter", chapter)
-      createPDF(title, chapter, path)
-
-    print("\n  Finished download request")
     
-    if not yesOrNo("  Download more? "):
+    search = inputString("\n\tEnter Series Name: ")
+    searchResults = searchForManga(search) #tuple(mangaNames, mangaChapters)
+    if len(searchResults[0]) == 0:
+      clearScreen()
+      drawHeader()
+      print("\n  No search results found for", search, " :(")
+      continue
+    
+    index = inputInteger("\n  Please select manga index from above: ", 0, len(searchResults[0])-1)
+    seriesName = searchResults[0][index]
+    totalChapters = int(searchResults[1][index])
+    clearScreen()
+    drawHeader()
+
+    print("\n  Selected [" + addZeroes(str(index), 3) + "] - " + seriesName + " --- " + str(totalChapters) + " Chapters")
+    chapterStart = inputInteger("\tEnter Chapter Range Start : ", 1, totalChapters)
+    chapterEnd = inputInteger("\tEnter Chapter Range End : ", chapterStart, totalChapters) 
+    print ("\tDownloading Chapter(s)", chapterStart, "-", chapterEnd, "\n")
+
+    for chapter in range(chapterStart, chapterEnd + 1):
+      try:
+        downloadMangaChapter(seriesName, chapter, path, chapterEnd + 1) 
+      except KeyboardInterrupt:
+        endProgram("\n  Keyboard Interruption detected.\n  Download interrupted.\n  CBZ file was not created.")
+      createCBZ(seriesName, chapter, path)
+    if not yesOrNo("\n\n  Download Request Finished. Would you like to download more? "):
       break 
+    else:
+      clearScreen()
+      drawHeader()
+
+
 
 # Run main program loop
 main()
-
-
-
